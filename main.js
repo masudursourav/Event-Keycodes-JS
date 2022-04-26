dec3e0eada854e6f9ad2f4a0822f98be206e631b
masudursourav <masudursourav@ieee.org>
2022-04-26 15:20:01 +0600

    26/04/22

---
+++ main.js
@@ -0,0 +1,18 @@
+window.addEventListener('keydown', (e)=>{
+    const insert = document.getElementById('insert');
+    insert.innerHTML = `
+    <div class="key">
+    ${e.key === ' ' ? 'Space' : e.key}
+    <small >event.key</small>
+ </div>
+<div class="key">
+    ${e.keyCode}
+    <small>event.keyCode</small>
+ </div>
+<div class="key">
+    ${e.code}
+    <small>event.code</small>
+ </div>
+    `
+
+});
